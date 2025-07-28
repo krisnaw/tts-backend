d05a99050af395d53267b87d93eb6570ae15af5f
krisna <krisna.w2010@gmail.com>
2025-07-28 19:43:00 +0800

    wip

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,6 @@
-import {integer, pgTable, text, timestamp, varchar} from "drizzle-orm/pg-core";
+import {integer, numeric, pgTable, text, timestamp, varchar} from "drizzle-orm/pg-core";
 import {createInsertSchema, createSelectSchema} from "drizzle-zod";
+import {relations} from "drizzle-orm";
 
 export const users = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
@@ -35,9 +36,24 @@ export const loginUserSchema = insertUsersSchema.partial()
       password: true
     })
 
-export const recorder = pgTable("recorder", {
+export const records = pgTable("records", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   content: text().notNull(),
+  pitch: numeric('pitch', { precision: 5, scale: 2 }).default('1.0').notNull(), // Default 1.0, range 0.0 to 2.0
+  volume: numeric('volume', { precision: 5, scale: 2 }).default('1.0').notNull(), // Default 1.0, range 0.0 to 1.0
+  rate: numeric('rate', { precision: 5, scale: 2 }).default('1.0').notNull(),
+  voice: text().notNull(),
+  userId: integer('user_id').notNull(),
   createdAt: timestamp().defaultNow(),
   updatedAt: timestamp().defaultNow()
 })
+
+export const recordsRelations = relations(records, ({ one }) => ({
+  author: one(users, {
+    fields: [records.userId],
+    references: [users.id],
+  }),
+}));
+
+export const selectRecordsSchema = createSelectSchema(records);
+export const insertRecordsSchema = createInsertSchema(records);
