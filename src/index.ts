03ee10f6a4cb779d84bf0826c39e67ef4c65bbe3
krisna <krisna.w2010@gmail.com>
2025-07-28 16:06:57 +0800

    Init

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,10 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+// eslint-disable-next-line no-console
+console.log(`Server is running on port http://localhost:3000`);
+
 serve({
   fetch: app.fetch,
   port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+});
